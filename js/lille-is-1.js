8bb46fee1aa96c3f25844266a53ea406f4675d6d
Celine <celine.ce.holst@gmail.com>
2020-11-30 10:35:00 +0100

    header + footer

--- js/lille-is-1.js
+++ js/lille-is-1.js
@@ -1,9 +1,21 @@
-/*
- * fil: js.js
- */
-
 console.log('file: js/js.js loaded');
 
 $(document).ready(function () {
-
+    
+    /* Vis navbar */
+    const showMenu = (toggleId, navId) => {
+        const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+        
+        if(toggle && nav) {
+            toggle.addEventListener('click', () => {
+               nav.classList.toggle('show')
+                /* Ændre toggle ikon */
+                toggle.classList.toggle('bx-x')
+            });
+        }
+    }
+    
+    showMenu('header-toggle','nav-menu');
+    
 });
